dcf309667f086760ee87c197c76ed07b56ab60b6
bscpaz <soujava@gmail.com>
2021-10-19 15:59:56 -0300

    First Version

---
+++ src/main/java/br/com/bscpaz/threadscript/ThreadScriptApplication.java
@@ -0,0 +1,13 @@
+package br.com.bscpaz.threadscript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadScriptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadScriptApplication.class, args);
+	}
+
+}
